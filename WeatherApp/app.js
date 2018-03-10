5425565989102093c19cd0bdd8189c46087d937e
razekei <iirayy@live.com>
2018-03-09 16:19:27 -0800

    read and write to search history file

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,28 +1,50 @@
 const
     weather = require('../MetaWeatherAPI/index')
-    // inquirer = require('inquirer')
+// inquirer = require('inquirer')
 
 const locationWeather = (location) => {
     weather.woeid_by_query(location)
-        .then(result=>{
+        .then(result => {
             console.log(result)
         })
         .catch(err => console.log(err))
 }
 
-const lattLongWeather = (latt,long) => {
-    weather.woeid_by_lattlong(latt,long)
-        .then(result=>{
+const lattLongWeather = (latt, long) => {
+    weather.woeid_by_lattlong(latt, long)
+        .then(result => {
             console.log(result)
         })
         .catch(err => console.log(err))
 }
 
 const history = () => {
-    //add method here
+    const fs = require('fs');
+
+    function createFile(filename) {
+        fs.open(filename, 'weatherSearchHistory.json', function (err, fd) {
+            if (err) {
+                fs.writeFile(filename, 'weatherSearchHistory.json', function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            } else {
+                fs.readFile('./weatherSearchHistory.json', 'utf-8', function (err, historyData) {
+                    if (err) throw err
+                    var data = JSON.parse(historyData);
+                });
+                //console.log(data);
+                fs.writeFile('./weatherSearchHistory.json', JSON.stringify(data), 'utf-8', function (err) {
+                    if (err) throw err;
+                })
+            }
+        });
+    }
 }
 
+
 module.exports = {
-    locationWeather,lattLongWeather,
+    locationWeather, lattLongWeather,
     history
 }
