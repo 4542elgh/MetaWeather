581dd98069a2460d2f12f607cc1c5ebe02bcb129
razekei <iirayy@live.com>
2018-03-14 00:20:26 -0700

    completed read & write search history to local file

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -6,6 +6,7 @@ const locationWeather = (location) => {
     weather.woeid_by_query(location)
         .then(result => {
             console.log(result)
+            history(result)
         })
         .catch(err => console.log(err))
 }
@@ -18,31 +19,63 @@ const lattLongWeather = (latt, long) => {
         .catch(err => console.log(err))
 }
 
-const history = () => {
+const history = (result) => {
     const fs = require('fs');
+    const filename = 'weatherSearchHistory.json';
 
-    function createFile(filename) {
-        fs.open(filename, 'weatherSearchHistory.json', function (err, fd) {
+    function createFile(result) {
+        fs.open(filename, 'r', function (err, fd) {
+
+            //create file if it doesn't already exist
             if (err) {
-                fs.writeFile(filename, 'weatherSearchHistory.json', function (err) {
+                fs.writeFile(filename, JSON.stringify(result), (err) => {
                     if (err) {
                         console.log(err);
                     }
+                    console.log("Search history file not found. Search history file has been created.");
+                    console.log("Seach result saved to serch history file.\nCurrently saved results: " + result.length + "\nMax number of saved resutls is 5.");
                 });
-            } else {
-                fs.readFile('./weatherSearchHistory.json', 'utf-8', function (err, historyData) {
-                    if (err) throw err
-                    var data = JSON.parse(historyData);
-                });
-                //console.log(data);
-                fs.writeFile('./weatherSearchHistory.json', JSON.stringify(data), 'utf-8', function (err) {
-                    if (err) throw err;
+            }
+
+            //add to existing file if search history file already exists
+            else {
+                fs.readFile(filename, (err, data) => {
+
+                    //read existing search results from file
+                    var historyDataArray = JSON.parse(data);
+
+                    //check for duplicate searched city before adding it back to the bottom of the list
+                    for (var x = 0; x < historyDataArray.length; x++) {
+                        if (result[0].woeid == historyDataArray[x].woeid) {
+                            historyDataArray.splice(x, 1);
+                        }
+                    }
+
+                    //only saving 5 most recent searches so this deletes the oldest search history if there is already 5 results in saved history
+                    if (historyDataArray.length > 4) {
+                        historyDataArray.splice(0, 1);
+                    }
+
+                    //add newest search result to array
+                    historyDataArray.push(result[0]);
+
+                    //write updated array to file
+                    fs.writeFile(filename, JSON.stringify(historyDataArray), (err) => {
+                        console.log("Seach result saved to serch history file.\nCurrently saved results: " + historyDataArray.length + "\nMax number of saved resutls is 5.");
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                    if (err) {
+                        console.log(err);
+                    }
                 })
             }
-        });
-    }
-}
 
+        })
+    }
+    createFile(result);
+}
 
 module.exports = {
     locationWeather, lattLongWeather,
