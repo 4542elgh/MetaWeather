2bbedb4465f276ab9ab0f1e60778254bef9d0643
4542elgh <4542elgh@gmail.com>
2018-03-14 01:24:07 -0700

    add main menu loop and base structure

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,6 +1,7 @@
 const
     weather = require('../MetaWeatherAPI/index'),
-    CLI = require('./InteractiveCLI');
+    CLI = require('./InteractiveCLI'),
+    inquirer = require('inquirer');
 
 const locationWeather_woeid = (location) => {
     weather.woeid_by_query(location)
@@ -17,37 +18,38 @@ const locationWeather_woeid = (location) => {
                             // console.log(result[0].woeid)
                             weather.get_weather_by_woeid(result[0].woeid)
                                 .then(result=>{
-                                   print(result.consolidated_weather)
+                                   print(result.consolidated_weather);
+                                    menu_recur()
                                 })
                         })
                 })
         })
         .catch(err => console.log(err))
 }
-
-const lattLongWeather_woeid = (latt,long) => {
-    weather.woeid_by_lattlong(latt,long)
-        .then(result=>{
-            let array = [];
-            result.forEach(item=>{
-                array.push(item.title);
-            })
-
-            CLI.interactiveCLI(array)
-                .then(result=>{
-                   weather.woeid_by_query(result.location)
-                       .then(result=>{
-                           // console.log(result[0].woeid)
-                           weather.get_weather_by_woeid(result[0].woeid)
-                               .then(result=>{
-                                   console.log(result)
-                               })
-                       })
-                })
-            // console.log(result);
-        })
-        .catch(err => console.log(err))
-}
+//
+// const lattLongWeather_woeid = (latt,long) => {
+//     weather.woeid_by_lattlong(latt,long)
+//         .then(result=>{
+//             let array = [];
+//             result.forEach(item=>{
+//                 array.push(item.title);
+//             })
+//
+//             CLI.interactiveCLI(array)
+//                 .then(result=>{
+//                    weather.woeid_by_query(result.location)
+//                        .then(result=>{
+//                            // console.log(result[0].woeid)
+//                            weather.get_weather_by_woeid(result[0].woeid)
+//                                .then(result=>{
+//                                    console.log(result)
+//                                })
+//                        })
+//                 })
+//             // console.log(result);
+//         })
+//         .catch(err => console.log(err))
+// }
 
 const print = (result)=>{
     result.forEach(item=>{
@@ -59,6 +61,55 @@ const FtoC=(F)=>{
     return (F*(9/5)+32);
 }
 
+const ui = ()=>{
+    return inquirer.prompt([{
+        type:'input',
+        message:'Enter your location:',
+        name:'location',
+        validate:(choices)=>{
+            if(choices>1 || choices<0){
+                return false;
+            }
+            else{
+                return true
+            }
+        }
+    }])
+}
+
+const menu = ()=>{
+    return inquirer.prompt([{
+        type:'list',
+        message:'Menu:',
+        name:'option',
+        choices:['history','exit'],
+        validate:(choices)=>{
+            if(choices>1 || choices<0){
+                return false;
+            }
+            else{
+                return true
+            }
+        }
+    }])
+}
+
+const menu_recur = ()=>{
+        menu().then(result=>{
+            switch(result.option){
+                case 'history' : {
+                    ui().then(result=>{
+                        locationWeather_woeid(result.location)
+                    })
+                    break;
+                }
+                case 'exit' : {
+                    process.exit(0)
+                    break;
+                }
+            }
+    })
+}
 module.exports = {
-    locationWeather_woeid,lattLongWeather_woeid
+    menu_recur
 }
--- WeatherApp/cli.js
+++ WeatherApp/cli.js
@@ -3,43 +3,45 @@ const
     yargs = require('yargs')
 
 const flags = yargs.usage('$0: Usage <cmd> [options]')
+    // .command({
+    //     command:'search',
+    //     desc: '-- Get WOEID at a specific location \n -l -- Specify location by "city"',
+    //     handler: (argv) => {
+    //         app.locationWeather_woeid(argv.location)
+    //     }
+    // })
+    //
+    // .command({
+    //     command:'weatherLocation',
+    //     desc: '-- Get WOEID at a specific location \n -l -- Specify location by "city"',
+    //     builder:(yargs)=>{
+    //         return yargs.option('l',{
+    //             alias : 'location',
+    //             desc: '-- Getting weather for specific location'
+    //         })
+    //     },
+    //     handler: (argv) => {
+    //         app.locationWeather_woeid(argv.location)
+    //     }
+    // })
+    // .command({
+    //     command:'weatherLattLong',
+    //     desc: '-- Get WOEID at a specific latitude and longitude \n -l -- Specify location by "latt,long"',
+    //     builder:(yargs)=>{
+    //         return yargs.option('l',{
+    //             alias : 'lattlong',
+    //             desc: '-- Getting weather for specific location'
+    //         })
+    //     },
+    //     handler: (argv) => {
+    //         app.lattLongWeather_woeid(argv.lattlong)
+    //     }
+    // })
     .command({
-        command:'search',
-        desc: '-- Get WOEID at a specific location \n -l -- Specify location by "city"',
-        builder:(yargs)=>{
-            return yargs.option('l',{
-                alias : 'location',
-                desc: '-- Getting weather for specific location'
-            })
-        },
-        handler: (argv) => {
-            app.locationWeather_woeid(argv.location)
-        }
-    })
-    .command({
-        command:'weatherLocation',
-        desc: '-- Get WOEID at a specific location \n -l -- Specify location by "city"',
-        builder:(yargs)=>{
-            return yargs.option('l',{
-                alias : 'location',
-                desc: '-- Getting weather for specific location'
-            })
-        },
-        handler: (argv) => {
-            app.locationWeather_woeid(argv.location)
-        }
-    })
-    .command({
-        command:'weatherLattLong',
-        desc: '-- Get WOEID at a specific latitude and longitude \n -l -- Specify location by "latt,long"',
-        builder:(yargs)=>{
-            return yargs.option('l',{
-                alias : 'lattlong',
-                desc: '-- Getting weather for specific location'
-            })
-        },
-        handler: (argv) => {
-            app.lattLongWeather_woeid(argv.lattlong)
+        command:'Menu',
+        desc: '-- MetaWeather Main Menu"',
+        handler: () => {
+            app.menu_recur()
         }
     })
     .help()
