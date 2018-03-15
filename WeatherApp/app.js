91e9307d455e78f3c6f404e69eafd871df8e4615
4542elgh <4542elgh@gmail.com>
2018-03-14 17:14:16 -0700

    es6 and inquirer prompt with most recent at top

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,13 +1,13 @@
 const
     weather = require('../MetaWeatherAPI/index'),
     filename = 'weatherSearchHistory.json',
-    fs = require('fs');
-// inquirer = require('inquirer')
+    fs = require('fs'),
+    inquirer = require('inquirer')
 
 const locationWeather = (location) => {
     weather.woeid_by_query(location)
         .then(result => {
-            console.log(result)
+            // console.log(result)
             history(result)
         })
         .catch(err => console.log(err))
@@ -22,59 +22,64 @@ const lattLongWeather = (latt, long) => {
 }
 
 const history = (result) => {
+    createFile(result);
+}
 
-    function createFile(result) {
-        fs.open(filename, 'r', function (err, fd) {
+const createFile = (result)=>{
+    fs.open(filename, 'r', function (err, fd) {
 
-            //create file if it doesn't already exist
-            if (err) {
-                fs.writeFile(filename, JSON.stringify(result), (err) => {
-                    if (err) {
-                        console.log(err);
+        //create file if it doesn't already exist
+        if (err) {
+            fs.writeFile(filename, JSON.stringify(result), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("Search history file not found. Search history file has been created.");
+                console.log("Seach results saved: " + result.length + " of 5.");
+            });
+        }
+
+        //add to existing file if search history file already exists
+        else {
+            fs.readFile(filename, (err, data) => {
+
+                //read existing search results from file, if file empty, avoid parsing empty json
+                let historyDataArray = [];
+                try{
+                    historyDataArray = JSON.parse(data)
+                }catch(SyntaxError) {
+                    historyDataArray =[]
+                }
+
+                //check for duplicate entry
+                historyDataArray.forEach((item,index)=>{
+                    if (result[0].woeid == item.woeid){
+                        historyDataArray.splice(index,1)
                     }
-                    console.log("Search history file not found. Search history file has been created.");
-                    console.log("Seach results saved: " + result.length + " of 5.");
-                });
-            }
+                })
 
-            //add to existing file if search history file already exists
-            else {
-                fs.readFile(filename, (err, data) => {
+                //only saving 5 most recent searches so this deletes the oldest search history if there is already 5 results in saved history
+                if (historyDataArray.length > 4) {
+                    historyDataArray.splice(0, 1);
+                }
 
-                    //read existing search results from file
-                    var historyDataArray = JSON.parse(data);
+                //add newest search result to array
+                historyDataArray.push(result[0]);
 
-                    //check for duplicate searched city before adding it back to the bottom of the list
-                    for (var x = 0; x < historyDataArray.length; x++) {
-                        if (result[0].woeid == historyDataArray[x].woeid) {
-                            historyDataArray.splice(x, 1);
-                        }
-                    }
-
-                    //only saving 5 most recent searches so this deletes the oldest search history if there is already 5 results in saved history
-                    if (historyDataArray.length > 4) {
-                        historyDataArray.splice(0, 1);
-                    }
-
-                    //add newest search result to array
-                    historyDataArray.push(result[0]);
-
-                    //write updated array to file
-                    fs.writeFile(filename, JSON.stringify(historyDataArray), (err) => {
-                        console.log("Seach results saved: " + historyDataArray.length + " of 5.");
-                        if (err) {
-                            console.log(err);
-                        }
-                    })
+                //write updated array to file
+                fs.writeFile(filename, JSON.stringify(historyDataArray), (err) => {
+                    console.log("Seach results saved: " + historyDataArray.length + " of 5.");
                     if (err) {
                         console.log(err);
                     }
                 })
-            }
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
 
-        })
-    }
-    createFile(result);
+    })
 }
 
 const displayHistory = () => {
@@ -83,9 +88,33 @@ const displayHistory = () => {
             console.log("You can't display search history when nothing has been searched!! ):<")
         }
         else {
-            var historyDataArray = JSON.parse(data);
-            for (var x = 0; x < historyDataArray.length; x++) {
-                console.log(historyDataArray[x])
+            let historyDataArray = JSON.parse(data);
+            inquirerDisplay(historyDataArray).then(result=>{
+                //deal with inquirer selected option
+            })
+        }
+    })
+}
+
+const inquirerDisplay = (choices) =>{
+    let choice = [];
+
+    //fetch so top is most recent
+    choices.forEach((item,index)=>{
+        choice[choices.length-1-index]=item.title
+    })
+
+    return inquirer.prompt({
+        type:'list',
+        message:'Select History',
+        name:'location',
+        choices:choice,
+        validate:(choice)=>{
+            if(choices>1 || choices<0){
+                return false;
+            }
+            else{
+                return true
             }
         }
     })
