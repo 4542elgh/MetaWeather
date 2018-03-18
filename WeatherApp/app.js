e6cd69906ca5e2e8179662f7a2ead961477bc3d6
4542elgh <4542elgh@gmail.com>
2018-03-18 15:45:10 -0700

    lower case and return to menu if no result

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -142,6 +142,7 @@ const searchWeather = (cities, weather)=>{
 
     if(result.length === 0){
         console.log('Sorry there are no results for the miles and weather condition specified.')
+        return menu_recur()
     }
     else{
         print(result)
